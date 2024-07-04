9743f93c6ba0e201cae720642fc482089aed36f2
jwaminseok <109052897+jms0324@users.noreply.github.com>
2024-07-04 19:37:52 +0900

    [Gold III] Title: 흩날리는 시험지 속에서 내 평점이 느껴진거야, Time: 312 ms, Memory: 23260 KB -BaekjoonHub

---
+++ 백준/Gold/17951. 흩날리는 시험지 속에서 내 평점이 느껴진거야/흩날리는 시험지 속에서 내 평점이 느껴진거야.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int arr[];
+    static int N;
+    static int M;
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+        st=new StringTokenizer(br.readLine());
+        arr=new int[N];
+
+        for(int i=0;i<N;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+
+        }
+
+        bw.write(String.valueOf(BinarySearch(0,2000000)));
+
+
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+
+    static int BinarySearch(int left,int right){
+          //그룹의 수를 새주는 변수
+
+        while(left<=right){
+            int mid=(left+right)/2;
+            int sum=0;
+            int gnt=0;
+            for(int i=0;i<N;i++){
+                sum+=arr[i];
+                if(sum>=mid){
+                    gnt++;
+                    sum=0;
+                }
+
+            }
+            if(gnt>=M){
+                left=mid+1;
+            }
+            else if(gnt<M){
+                right=mid-1;
+            }
+
+
+        }
+        return right;
+
+
+    }
+
+
+}
