5ef2d1a2eb53b0741c3d0637b424d90ff19de4a7
jwaminseok <109052897+jms0324@users.noreply.github.com>
2024-07-11 20:56:17 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14916 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,55 +1,79 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	
-	static int[] seq;
-	static Integer[] dp;
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		seq = new int[N];
-		dp = new Integer[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		for(int i = 0; i < N; i++) {
-			seq[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		// 0 ~ N-1 까지 탐색 
-		for(int i = 0; i < N; i++) {
-			LIS(i);
-		}
-		
-		int max = dp[0];
-		// 최댓값 찾기 
-		for(int i = 1; i < N; i++) {
-			max = Math.max(max, dp[i]);
-		}
-		System.out.println(max);
-	}
-	
-	
-	static int LIS(int N) {
-		
-		// 만약 탐색하지 않던 위치의 경우 
-		if(dp[N] == null) {
-			dp[N] = 1;	// 1로 초기화 
-			
-			// N-1 부터 0까지중 N보다 작은 값들을 찾으면서 재귀호출. 
-			for(int i = N - 1; i >= 0; i--) {
-				if(seq[i] < seq[N]) {
-					dp[N] = Math.max(dp[N], LIS(i) + 1);
-				}
-			}
-		}
-		return dp[N];
-	}
+    static int dp[];
+    static int arr[];
+
+
+
+
+
+
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int N=Integer.parseInt(br.readLine());
+        arr=new int[N];
+        dp=new int[N];
+        StringTokenizer st=new StringTokenizer(br.readLine()," ");
+
+        for(int i=0;i<N;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+
+        }
+
+        for(int i=0;i<N;i++){
+            topdown(i);
+        }
+
+        int max=dp[0];
+        for(int i=1;i<N;i++){
+            max=Math.max(dp[i],max);
+        }
+
+        bw.write(String.valueOf(max));
+        bw.flush();
+        bw.close();
+        br.close();
+
+
+
+
+    }
+
+    static int topdown(int n){
+        if(dp[n]==0){
+            dp[n]=1;
+
+            for(int i=n-1;i>=0;i--){
+                if(arr[i]<arr[n]){
+                    dp[n]=Math.max(dp[n],topdown(i)+1);
+                }
+            }
+
+
+
+        }
+
+
+
+        return dp[n];
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
