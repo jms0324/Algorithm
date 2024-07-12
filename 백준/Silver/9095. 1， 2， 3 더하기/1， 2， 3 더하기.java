aa321257689f29a3ac05f2b5b9a91154a9e3c223
jwaminseok <109052897+jms0324@users.noreply.github.com>
2024-07-13 01:07:43 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -7,32 +7,30 @@ public class Main {
 
 
 
-    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
         int N=Integer.parseInt(br.readLine());
-        for(int k=0;k<N;k++){
-
+        for(int i=0;i<N;i++){
             int n=Integer.parseInt(br.readLine());
-            int dp[]=new int[n+1];
             if(n==1 || n==2){
                 bw.write(String.valueOf(n));
                 bw.newLine();
+                continue;
 
             }
-            else{
-                dp[1]=1;
-                dp[2]=2;
-                dp[3]=4;
-                for(int i=4;i<=n;i++){
-                    dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
-                }
-                bw.write(String.valueOf(dp[n]));
-                bw.newLine();
+            int dp[]=new int[n+1];
 
+            dp[1]=1;
+            dp[2]=2;
+            dp[3]=4;
+
+            for(int j=4;j<=n;j++){
+                dp[j]=dp[j-1]+dp[j-2]+dp[j-3];
             }
+            bw.write(String.valueOf(dp[n]));
+            bw.newLine();
 
 
 
@@ -41,13 +39,7 @@ public class Main {
 
 
 
-
-
-
-
-
         bw.flush();
-        br.close();
         bw.close();
 
 
@@ -58,6 +50,8 @@ public class Main {
 
 
     }
+
+
 
 
 
