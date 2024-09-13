0cc42adf7fbdb6a9a3e72cc005b7e933d685552e
jwaminseok <109052897+jms0324@users.noreply.github.com>
2024-09-13 11:06:11 +0900

    [Silver I] Title: 그림, Time: 344 ms, Memory: 48724 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,71 +1,84 @@
 import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
-
-public class Main{
-    static int [] dx={1,0,-1,0};
-    static int [] dy={0,1,0,-1};
-
-    public static void main(String[] args) throws IOException{
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
+    public static void main(String[] args) throws IOException {
 
 
-        String arr=br.readLine();
-        int a=Integer.parseInt(arr.split(" ")[0]);
-        int b=Integer.parseInt(arr.split(" ")[1]);
-
-        int [][] matrix= new int[a][b];
-        boolean [][] visited=new boolean[a][b];
-        Queue<int []>queue=new LinkedList<>();
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 
 
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        int a=Integer.parseInt(st.nextToken());
+        int b=Integer.parseInt(st.nextToken());
+
+        int [][] matrix=new int[a][b];
+        boolean [][] visit =new boolean[a][b];
+        int num=0;
+        int max=0;
+
+        int divx[]={1,0,-1,0};
+        int divy[]={0,1,0,-1};
+
+        Queue<int []> queue=new LinkedList<>();
 
         for(int i=0;i<a;i++){
-            StringTokenizer st=new StringTokenizer(br.readLine());
+            st=new StringTokenizer(br.readLine());
+
 
             for(int j=0;j<b;j++){
+
                 matrix[i][j]=Integer.parseInt(st.nextToken());
+
 
             }
         }
 
-        int num=0;
-        int max=0;
-
         for(int i=0;i<a;i++){
+
             for(int j=0;j<b;j++){
 
-                if(visited[i][j] || matrix[i][j]==0){
+                if(visit[i][j] || matrix[i][j]==0){
                     continue;
+
                 }
-                num++; //그림하나나옴
 
-                visited[i][j]=true;
+                num++;
+                visit[i][j]=true;
+                queue.add(new int[]{i,j});
 
-                queue.add(new int[] {i,j});
                 int area=0;
 
                 while(!queue.isEmpty()){
                     area++;
 
-                    int[] cur= queue.poll();
-                    for(int dir=0;dir<4;dir++){
-                        int nx=cur[0]+dx[dir];
-                        int ny=cur[1]+dy[dir];
-                        if(nx<0 || nx>=a || ny<0 || ny >= b){
+
+                    int []cur =queue.poll();
+
+                    for(int dir=0; dir<4 ;dir++){
+                        int dx=cur[0]+divx[dir];
+                        int dy=cur[1]+divy[dir];
+
+                        if(dx<0 || dx>=a || dy<0 || dy>=b){
+
                             continue;
                         }
-                        if(visited[nx][ny] || matrix[nx][ny] != 1){
+                        if(visit[dx][dy]==true || matrix[dx][dy] != 1){
                             continue;
                         }
-                        visited[nx][ny]=true;
-                        queue.add(new int[]{nx,ny});
+                        visit[dx][dy]=true;
+                        queue.add(new int[] {dx,dy});
+
+
+
 
                     }
+
+
                 }
                 max=Math.max(max,area);
 
@@ -78,26 +91,12 @@ public class Main{
         bw.newLine();
         bw.write(String.valueOf(max));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
         bw.flush();
         bw.close();
-        br.close();
+
+
+
+
+
     }
-
-
-
-
-
 }
