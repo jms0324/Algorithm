bf879672c2facfce7b216cb3216c1b7419cf9d8e
jwaminseok <109052897+jms0324@users.noreply.github.com>
2024-09-26 16:31:14 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14676 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,68 +1,156 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
+
+    static int N;
+    static int M;
+    static int horse;
+    static char[][] board;
+    static int[][] visited;
+
+    static int[] dx={0,0,-1,1};
+    static int[] dy={1,-1,0,0};
+
+
+
+
+
+
+
+
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st=new StringTokenizer(br.readLine());
 
-        int a=Integer.parseInt(st.nextToken());
-        int b=Integer.parseInt(st.nextToken());
 
-        int matrix[][]=new int[a][b];
-        int visited[][]=new int[a][b];
-        int dx[]=new int[]{1,0,-1,0};
-        int dy[]=new int[]{0,1,0,-1};
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+        board=new char[N][M];
+        visited=new int[N][M];
 
-        for(int i=0;i<a;i++){
-
+        for(int i=0;i<N;i++){
             String str=br.readLine();
-            for(int j=0;j<b;j++){
-                matrix[i][j]=str.charAt(j)-'0';
-
+            for(int j=0;j<M;j++){
+                board[i][j]=str.charAt(j);
                 visited[i][j]=0;
-
-
-
             }
         }
+        bw.write(String.valueOf(BFS()));
 
-        Queue<int []> queue =new LinkedList<>();
-        queue.add(new int[]{0,0});
-        visited[0][0]++;
 
-        while(!queue.isEmpty()){
 
-            int cur[]=queue.poll();
 
-            for(int i=0;i<4;i++){
-                int nextX=cur[0]+dx[i];
-                int nextY=cur[1]+dy[i];
 
-                if(nextX<0 || nextX>=a || nextY<0 || nextY >= b)continue;
-                //범위확인
-                if(matrix[nextX][nextY]!=1 || visited[nextX][nextY]!=0)continue;
-                //1인 지점제외 , 이미 거리가 계산된 지점은 제외
 
-                queue.add(new int[]{nextX,nextY});
-                visited[nextX][nextY]=visited[cur[0]][cur[1]]+1;
 
-            }
-        }
 
-        bw.write(String.valueOf(visited[a-1][b-1]));
+
+
+
 
 
 
 
 
         bw.flush();
-        br.close();
         bw.close();
 
+
+
+
+
+
     }
+
+    static int BFS(){
+
+        Queue<int []> queue =new LinkedList<>();
+
+        queue.add(new int[]{0,0});
+        visited[0][0]=1;
+
+        while(!queue.isEmpty()){
+
+            int cur[]= queue.poll();
+
+            if(cur[0]==N-1 && cur[1]==M-1)return visited[cur[0]][cur[1]];
+
+            for(int i=0;i<4;i++){
+
+                int nx=cur[0]+dx[i];
+                int ny=cur[1]+dy[i];
+
+                if(nx<0 || nx>=N || ny<0 || ny>=M)continue;
+                if(visited[nx][ny]!=0 || board[nx][ny]=='0')continue;
+
+                visited[nx][ny]=visited[cur[0]][cur[1]]+1;
+
+                queue.add(new int[]{nx,ny});
+            }
+
+        }
+
+        return visited[N-1][M-1];
+
+
+
+
+    }
+
+   /* static int DFS(int x, int y){
+
+        Stack<int[]> stack = new Stack<>();
+
+
+        visited[x][y]=true;
+
+        stack.add(new int[]{x,y});
+        int area=0;
+
+        while(!stack.isEmpty()){
+
+            int cur[]=stack.pop();
+            area++;
+
+
+            for(int i=0;i<4;i++){
+
+                int nx=cur[0]+dx[i];
+                int ny=cur[1]+dy[i];
+
+                if(nx<0 || nx>=N || ny<0 || ny >=M)continue;  //범위 넘어가면
+                if(board[nx][ny]==0 || visited[nx][ny])continue;  //방문했거나 0이면
+
+                visited[nx][ny] = true; //boolean으로 해도됨
+
+
+
+                stack.add(new int[]{nx,ny});
+            }
+
+        }
+        return area;
+
+
+    }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
