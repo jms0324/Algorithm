aafbd4a561bae000888cfdf4066f877b0509e871
jwaminseok <109052897+jms0324@users.noreply.github.com>
2024-10-02 00:28:19 +0900

    [Gold V] Title: 적록색약, Time: 140 ms, Memory: 17236 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,116 +1,85 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
+
+    static char[][] board;
+
+    static int N;
+
+    static boolean[][] visited;
+
+    static int[] dx={0,-1,1,0};
+    static int[] dy={1,0,0,-1};
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        int N=Integer.parseInt(st.nextToken());
-        char board[][]=new char[N][N];
-        int visited[][]=new int[N][N];
-        int visited2[][]=new int[N][N];
+        N=Integer.parseInt(br.readLine());
+
+        board=new char[N][N];
+
+        visited=new boolean[N][N];
 
         for(int i=0;i<N;i++){
             String str=br.readLine();
+
             for(int j=0;j<N;j++){
                 board[i][j]=str.charAt(j);
-                visited[i][j]=-1;
-                visited2[i][j]=-1;
-
 
 
             }
         }
-        int []dx={1,0,-1,0};
-        int []dy={0,1,0,-1};
 
-        Queue<int []> queue=new LinkedList<>();
-        int num=0;
+        int cnt=0;
 
         for(int i=0;i<N;i++){
+
+
             for(int j=0;j<N;j++){
-
-
-                if(visited[i][j]!=-1) continue;
-                char want=board[i][j];
-                num++;
-
-                queue.add(new int[]{i,j});
-                visited[i][j]++;
-
-                while(!queue.isEmpty()){
-                    int cur[]= queue.poll();
-                    for(int dir=0;dir<4;dir++){
-
-                        int nextx=cur[0]+dx[dir];
-                        int nexty=cur[1]+dy[dir];
-
-                        if(nextx <0 || nextx>=N || nexty>=N || nexty <0) continue;
-                        if(board[nextx][nexty]!=want || visited[nextx][nexty]!=-1)continue;
-
-                        queue.offer(new int[]{nextx,nexty});
-                        visited[nextx][nexty]++;
-
-                    }
-
+                if(visited[i][j]!=true){
+                    DFS(i,j);
+                    cnt++;
                 }
 
 
             }
         }
-        bw.write(String.valueOf(num+" "));
+
+        bw.write(String.valueOf(cnt+" "));
 
 
-        num=0;
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(board[i][j]=='G'){
-                    board[i][j]='R';
-                }
-            }
-
-        }
-
+        visited=new boolean[N][N];
 
         for(int i=0;i<N;i++){
             for(int j=0;j<N;j++){
 
-
-                if(visited2[i][j]!=-1) continue;
-                char want=board[i][j];
-                num++;
-
-                queue.add(new int[]{i,j});
-                visited2[i][j]++;
-
-                while(!queue.isEmpty()){
-                    int cur[]= queue.poll();
-                    for(int dir=0;dir<4;dir++){
-
-                        int nextx=cur[0]+dx[dir];
-                        int nexty=cur[1]+dy[dir];
-
-                        if(nextx <0 || nextx>=N || nexty>=N || nexty <0) continue;
-                        if(board[nextx][nexty]!=want || visited2[nextx][nexty]!=-1)continue;
-
-                        queue.offer(new int[]{nextx,nexty});
-                        visited2[nextx][nexty]++;
-
-                    }
-
-                }
-
+                if(board[i][j]=='G')board[i][j]='R';
 
             }
         }
-        bw.write(String.valueOf(num));
-        bw.newLine();
+        cnt=0;
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+
+                if(!visited[i][j]){
+                    DFS(i,j);
+                    cnt++;
+
+                }
+
+            }
+
+        }
+
+        bw.write(String.valueOf(cnt));
+
+
+
 
 
 
@@ -120,5 +89,46 @@ public class Main {
         bw.close();
 
 
+
+
     }
+
+
+    static void DFS(int x,int y){
+
+
+
+        Stack<int []>stack=new Stack<>();
+        stack.push(new int[]{x,y});
+        visited[x][y]=true;
+
+        while(!stack.isEmpty()){
+
+            int cur[]=stack.pop();
+
+            for(int i=0;i<4;i++){
+                int nx=cur[0]+dx[i];
+                int ny=cur[1]+dy[i];
+
+                if(nx<0 || nx>=N || ny<0 || ny>=N)continue;
+                if(visited[nx][ny] || board[nx][ny]!=board[cur[0]][cur[1]])continue;
+
+                visited[nx][ny]=true;
+                stack.add(new int[]{nx,ny});
+
+
+            }
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
 }
