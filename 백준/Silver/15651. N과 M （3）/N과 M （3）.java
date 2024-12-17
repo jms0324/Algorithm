229719af605318dee0dfd3f0aed1316347b30e24
jwaminseok <109052897+jms0324@users.noreply.github.com>
2024-12-17 16:16:45 +0900

    [Silver III] Title: N과 M (3), Time: 524 ms, Memory: 164620 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class Main{
+
+    static BufferedReader br=new BufferedReader(new InputStreamReader(in));
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(out));
+
+    static boolean used[];
+    static int arr[];
+    static int N;
+    static int M;
+
+
+    public static void main(String[] args) throws IOException {
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+        used=new boolean[N+2];
+        arr=new int [N+2];
+
+        NM(0);
+
+
+
+        bw.flush();
+        bw.close();
+
+
+
+
+
+
+
+    }
+
+    static void NM(int k)throws IOException{
+        if(k==M){
+
+            for(int i=0;i<M;i++){
+                bw.write(String.valueOf(arr[i])+" ");
+
+            }
+            bw.newLine();
+            return;
+        }
+        for(int i=1;i<=N;i++){
+
+
+
+                arr[k]=i;
+
+                NM(k+1);
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
