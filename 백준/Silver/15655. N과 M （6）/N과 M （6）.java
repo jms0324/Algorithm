b90243b4fb8197f814bde81bdd44ee7328331782
jwaminseok <109052897+jms0324@users.noreply.github.com>
2024-12-18 21:04:10 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 15932 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main{
+
+    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int N;
+    static int[] arr;
+    static int[] result;
+    static boolean [] used;
+    static int M;
+    public static void main(String[] args) throws IOException {
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+        arr=new int[N+2];
+        result=new int[N];
+
+
+        st=new StringTokenizer(br.readLine());
+
+        for(int i=0;i<N;i++){
+            result[i]=Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(result);
+
+
+        dfs(0,0);
+
+        bw.flush();
+        bw.close();
+
+
+
+
+
+    }
+
+    public static void dfs(int depth,int start)throws IOException{
+
+        if(depth==M){
+            for(int i=0;i<M;i++){
+                bw.write(arr[i]+" ");
+
+            }
+            bw.newLine();
+            return;
+        }
+        for(int i=start;i<result.length;i++){
+
+
+
+                arr[depth]=result[i];
+                dfs(depth+1,i+1);
+
+
+
+        }
+
+
+
+    }
+}
