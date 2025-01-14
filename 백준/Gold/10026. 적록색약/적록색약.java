84f3213873881aa016e962ecccfd06f0d75a1636
jwaminseok <109052897+jms0324@users.noreply.github.com>
2025-01-14 17:39:27 +0900

    [Gold V] Title: 적록색약, Time: 128 ms, Memory: 15860 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,36 +1,30 @@
 import java.io.*;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Main {
+public class Main{
 
-
-    static char[][] board;
-
+    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
     static int N;
+    static char arr[][];
 
-    static boolean[][] visited;
+    static int dx[]={0,0,-1,1};
+    static int dy[]={1,-1,0,0};
 
-    static int[] dx={0,-1,1,0};
-    static int[] dy={1,0,0,-1};
-
+    static boolean visited[][];
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-
         N=Integer.parseInt(br.readLine());
-
-        board=new char[N][N];
-
+        arr=new char[N][N];
         visited=new boolean[N][N];
 
         for(int i=0;i<N;i++){
             String str=br.readLine();
 
-            for(int j=0;j<N;j++){
-                board[i][j]=str.charAt(j);
+            for(int j=0 ; j<N;j++){
 
-
+                arr[i][j]=str.charAt(j);
             }
         }
 
@@ -38,97 +32,95 @@ public class Main {
 
         for(int i=0;i<N;i++){
 
-
-            for(int j=0;j<N;j++){
-                if(visited[i][j]!=true){
-                    DFS(i,j);
-                    cnt++;
-                }
-
-
-            }
-        }
-
-        bw.write(String.valueOf(cnt+" "));
-
-
-        visited=new boolean[N][N];
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-
-                if(board[i][j]=='G')board[i][j]='R';
-
-            }
-        }
-        cnt=0;
-
-        for(int i=0;i<N;i++){
             for(int j=0;j<N;j++){
 
                 if(!visited[i][j]){
-                    DFS(i,j);
+                    BFS(i,j);
                     cnt++;
-
                 }
-
             }
+        }
 
+        bw.write(String.valueOf(cnt)+" ");
+
+        cnt = 0;
+
+        for(int i=0;i<N;i++){
+
+            for(int j=0;j<N;j++){
+
+                visited[i][j]=false;
+                if(arr[i][j]=='R')arr[i][j]='G';
+            }
+        }
+
+        for(int i=0;i<N;i++){
+
+            for(int j=0;j<N;j++){
+
+                if(!visited[i][j]){
+                    BFS(i,j);
+                    cnt++;
+                }
+            }
         }
 
         bw.write(String.valueOf(cnt));
 
 
-
-
-
-
-
-
         bw.flush();
         bw.close();
-
-
-
 
     }
 
 
-    static void DFS(int x,int y){
+    static void BFS(int x,int y){
 
-
-
-        Stack<int []>stack=new Stack<>();
-        stack.push(new int[]{x,y});
+        char want=arr[x][y];
+        Queue<Node> queue=new LinkedList<>();
+        queue.add(new Node(x,y));
         visited[x][y]=true;
 
-        while(!stack.isEmpty()){
-
-            int cur[]=stack.pop();
-
+        while(!queue.isEmpty()){
+            Node cur = queue.poll();
             for(int i=0;i<4;i++){
-                int nx=cur[0]+dx[i];
-                int ny=cur[1]+dy[i];
 
-                if(nx<0 || nx>=N || ny<0 || ny>=N)continue;
-                if(visited[nx][ny] || board[nx][ny]!=board[cur[0]][cur[1]])continue;
+
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if(check(nx,ny)) continue;
+                if(arr[nx][ny]!=want || visited[nx][ny] )continue;
 
                 visited[nx][ny]=true;
-                stack.add(new int[]{nx,ny});
+                queue.add(new Node(nx,ny));
+
 
 
             }
+
 
         }
 
 
 
 
-
-
-
     }
 
+    static boolean check(int x,int y){
 
+        if(x<0 || x>=N || y<0 || y>=N)return true;
+        return false;
+    }
 
+    static class Node{
+
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
