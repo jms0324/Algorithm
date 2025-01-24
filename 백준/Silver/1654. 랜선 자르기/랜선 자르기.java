9206b5c4bb28f2452f6d9a3d4e949271c297675a
jwaminseok <109052897+jms0324@users.noreply.github.com>
2025-01-24 23:46:59 +0900

    [Silver II] Title: 랜선 자르기, Time: 156 ms, Memory: 17264 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+ 
+		int K = Integer.parseInt(st.nextToken()); 
+		int N = Integer.parseInt(st.nextToken()); 
+ 
+		int[] arr = new int[K];
+		
+		long max = 0;
+		
+		// 입력과 동시에 해당 랜선의 길이가 최댓값인지를 확인하고 max를 갱신
+		for (int i = 0; i < K; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		
+ 
+		// 반드시 max에서 +1 값이어야 한다.
+		max++; 
+ 
+		long min = 0; 
+		long mid = 0; 
+ 
+		while (min < max) { 
+			
+		
+			mid = (max + min) / 2;
+			
+ 
+			long count = 0;
+			
+		
+			for (int i = 0; i < arr.length; i++) {
+				count += (arr[i] / mid);
+			}
+			
+			
+			if(count < N) {
+				max = mid;
+			}
+			else {
+				min = mid + 1;
+			}
+			
+ 
+		}
+		
+		// UpperBound로 얻어진 값(min)에 -1이 최대 길이
+		System.out.println(min - 1);
+	}
+}
