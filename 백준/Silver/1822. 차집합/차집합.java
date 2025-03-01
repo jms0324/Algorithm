10e09ca86032d356092a9681d36cf48c25f245c5
jwaminseok <109052897+jms0324@users.noreply.github.com>
2025-03-01 14:04:31 +0900

    [Silver IV] Title: 차집합, Time: 1272 ms, Memory: 157656 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.java
+++ 백준/Silver/1822. 차집합/차집합.java
@@ -5,62 +5,57 @@ import java.util.StringTokenizer;
 
 public class Main{
 
+    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int[] want;
-    static int[] notwant;
-
+    static int []arr;
+    static int []arr2;
     static int N;
     static int M;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
+
+
 
         StringTokenizer st=new StringTokenizer(br.readLine());
         N=Integer.parseInt(st.nextToken());
         M=Integer.parseInt(st.nextToken());
-        want=new int[N];
-        notwant=new int[M];
         st=new StringTokenizer(br.readLine());
+        arr=new int[N];
         for(int i=0;i<N;i++){
-            want[i]=Integer.parseInt(st.nextToken());
+            arr[i]=Integer.parseInt(st.nextToken());
+
         }
+
+        arr2=new int[M];
+
         st=new StringTokenizer(br.readLine());
-        for(int j=0;j<M;j++){
-            notwant[j]=Integer.parseInt(st.nextToken());
+        for(int i=0;i<M;i++){
+            arr2[i]=Integer.parseInt(st.nextToken());
+
         }
+        Arrays.sort(arr);
+        Arrays.sort(arr2);
+        int count=0;
+        ArrayList<Integer>answer = new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            if(BinarySearch(arr[i])==0){
+                count++;
+                answer.add(arr[i]);
 
 
-        Arrays.sort(want);
-        Arrays.sort(notwant);
-        int cnt=0;
-        ArrayList<Integer>stt=new ArrayList<>();
-
-        for(int i=0;i<want.length;i++){
-
-            if(BinarySearch(want[i],want)){
-                if(!BinarySearch(want[i],notwant)){
-                    cnt++;
-                    stt.add(want[i]);
-
-                }
             }
+
         }
-        if(cnt==0){
-            bw.write("0");
+        bw.write(String.valueOf(count));
+        bw.newLine();
+        if(count==0){
+            bw.flush();
+            return;
         }
-        else{
-            bw.write(cnt+"\n");
-            for(int i:stt){
-                bw.write(String.valueOf(i+" "));
-            }
+        for(int i:answer){
+            bw.write(String.valueOf(i+" "));
         }
-
-
-
-
-
-
 
         bw.flush();
         bw.close();
@@ -69,28 +64,19 @@ public class Main{
 
 
 
-
     }
 
-    static boolean BinarySearch(int target,int arr[]){
-        int low=0;
-        int hi=arr.length-1;
-        while(low<=hi){
+    static int BinarySearch(int target){
+        int st=0;
+        int en=arr2.length-1;
 
-            int mid=(low+hi)/2;
-            if(target==arr[mid])return true;
-            else if(target<arr[mid])hi=mid-1;
-            else{
-                low=mid+1;
-            }
+        while(st<=en){
+            int mid=(st+en)/2;
+            if(arr2[mid]==target)return 1;
+            if(arr2[mid]<target)st=mid+1;
+            if(arr2[mid]>target)en=mid-1;
+
         }
-
-        return false;
-
-
+        return 0;
     }
-
-
-
-
 }
