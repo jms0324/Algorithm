80c5245918a72aa9d1da680e6ec49f02bf2d807f
jwaminseok <109052897+jms0324@users.noreply.github.com>
2025-03-08 19:56:30 +0900

    [Silver II] Title: 나무 자르기, Time: 724 ms, Memory: 326296 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,36 +1,23 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main{
 
     static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-
-    static int []arr;
-    static int []arr2;
     static int N;
     static int M;
-
+    static int arr[];
     public static void main(String[] args) throws IOException {
-
-
-
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        N=Integer.parseInt(st.nextToken()); //나무의 수
-        M=Integer.parseInt(st.nextToken()); //나무의 길이
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
         arr=new int[N];
-        st = new StringTokenizer(br.readLine());
-        int max=-1;
+        st=new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++){
             arr[i]=Integer.parseInt(st.nextToken());
-            max=Math.max(max,arr[i]);
         }
-
-        bw.write(String.valueOf(BinarySearch(max)));
-
-
+        bw.write(String.valueOf(BinarySearch()));
         bw.flush();
         bw.close();
 
@@ -38,34 +25,33 @@ public class Main{
 
 
 
+
     }
 
-    static int BinarySearch(int en){
-        int st=1;
+    static int BinarySearch(){
+        int st=0;
+        int en=Integer.MAX_VALUE;
         while(st<=en){
-            Long cnt=0L;
             int mid=(st+en)/2;
+            Long cnt=0L;
 
-            for(int i=0;i<N;i++){
-                if(arr[i]-mid >0){
-                    cnt+=arr[i]-mid;  //자르고 난 길이
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]-mid>0){
+                    cnt+=arr[i]-mid;
 
                 }
 
             }
 
-            if(cnt<M){ //mid값이 크다 --> 덜 잘렸다
+            if(cnt<M){
                 en=mid-1;
             }
-            if(cnt>=M){  //mid값이 같다 --> 더 큰걸로 잘라본다 잘린길이가 너무많다 --> mid값이작다 M보다 커도 된다
+            else{
                 st=mid+1;
             }
         }
         return en;
 
     }
-
-
-
 
 }
