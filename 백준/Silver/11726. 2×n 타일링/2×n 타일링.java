c0feacbe8715a97193dffba4318df8f5c480e808
jwaminseok <109052897+jms0324@users.noreply.github.com>
2025-04-04 21:56:01 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,42 +1,36 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
 
+    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N;
+    static int M;
+    static int arr[];
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        N=Integer.parseInt(st.nextToken());
 
-
-
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static void main(String[] args) throws Exception{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-
-        int N=Integer.parseInt(br.readLine());
         if(N==1){
-            bw.write("1");
+            bw.write(String.valueOf(1));
             bw.flush();
             bw.close();
+
+
             return;
         }
-        int dp[]=new int[N+1];
-
-        dp[1]=1;
-        dp[2]=2;
+        arr=new int[N+1];
+        arr[1]=1;
+        arr[2]=2;
         for(int i=3;i<=N;i++){
-            dp[i]=(dp[i-1]+dp[i-2])%10007;
+            arr[i]=(arr[i-1]+arr[i-2])%10007;
+
         }
-        bw.write(String.valueOf(dp[N]));
 
-
-
-
-
-
-
-
-
-
-
+        bw.write(String.valueOf(arr[N]));
 
 
 
@@ -49,22 +43,7 @@ public class Main {
 
 
 
-
-
-
-
     }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
