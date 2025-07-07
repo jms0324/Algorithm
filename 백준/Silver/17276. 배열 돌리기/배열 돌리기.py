df5d351237acbe33c6e750c2582a62a1af352e22
jwaminseok <109052897+jms0324@users.noreply.github.com>
2025-07-07 15:56:08 +0900

    [Silver I] Title: 배열 돌리기, Time: 2608 ms, Memory: 52392 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
@@ -1,54 +1,47 @@
-n=int(input())
+time = int(input())
+for _ in range(time) :
+    N,angle = map(int,input().split())
+    basic=[list(map(int,input().split()))  for _ in range(N)]
 
-for _ in range(n) :
+    ans=[[0]* N for _ in range(N)]
 
-
-    N, angle=map(int,input().split())
-
-    basic=[list(map(int,input().split())) for i in range(N)]
-
-
-    ans=[[0] * N for _ in range(N)]  #답을 입력할배열
-    if(angle<0) :
-        angle=360+angle
-
-    if (angle == 360 or angle ==0) : #360일때
-        for i in basic:
-            print(*i) #unpack문법
-
+    if(angle ==0 or angle == 360) :
+        for i in basic :
+            print(*i)
     else :
-        for _ in range(angle//45) :
+        count=0
+        mid=N//2
+        if(angle<0) :
+            angle+=360
+            count=angle//45
+        else :
+            count=angle//45
 
-            mid=N//2
+
+        for k in range(count) :
+
+
             for i in range(N):
-                for j in range(N) :
+                for j in range(N):
 
-                    if(j==mid) :  #주대각선 -> 가운데열
-                        ans[i][j] = basic[i][i]
+                    if(i==j) :#주대각선 -> 가운데열
+                         ans[i][mid]=basic[i][j]
 
-                    elif (i + j == N - 1):  # 가운데열 -> 부대각선
-                        ans[i][j] = basic[i][mid]
+                    elif(j==mid) : #가운데열 -> 부대각선 i+j=N-1
+                        ans[i][N-i-1] = basic[i][j]
 
-                    elif (i == mid):  # 부대각선 -> 가운데행  이거 몰랏음
-                        ans[i][j] = basic[N - j - 1][j]
+                    elif(i+j==N-1) : #부대각선 -> 가운데행
+                        ans[mid][j] = basic[i][j]
 
-                    elif(i==j)  :  #가운데행 -> 주대각선
-                        ans[i][j] = basic[mid][j]
-
-
-                    else:
+                    elif(i==mid) : #basic의 가운데행 -> 주대각선 i가 2일때
+                        ans[j][j] = basic[i][j]
+                    else :
                         ans[i][j]=basic[i][j]
 
             for x in range(N):
-                for y in range(N) :
+                for y in range(N):
                     basic[x][y]=ans[x][y]
 
-        for k in basic:
-            print(*k)
-
-
-
-
-
-
+        for a in basic  :
+            print(*a)
 
