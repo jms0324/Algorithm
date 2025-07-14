017f7740db0175f93d8d23e086ad8102d2c9d567
jwaminseok <109052897+jms0324@users.noreply.github.com>
2025-07-14 12:46:21 +0900

    [Gold IV] Title: 이분 그래프, Time: 732 ms, Memory: 190268 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,52 +1,56 @@
 import sys
 input=sys.stdin.readline
-T=int(input())
 from collections import deque
+T=int(input())
 
+def BFS(start) :
+    check=True
 
-
-def BFS(start,visited) :
-    queue= deque()
+    queue=deque()
+    visited[start]=-1
     queue.append(start)
-    visited[start]=1
     while(queue) :
-
         cur=queue.popleft()
-
-        for i in list1[cur] :
-            if(visited[i]==0) : #아직 방문안한곳이라면
-                visited[i]=-1*visited[cur]
+        for i in graph[cur] :
+            # if(visited[i]!= 0 and visited[i]==visited[cur]) : #방문했는데 인접한 정점과 같을 경우 (이분그래프가 아님 )
+            #
+            #     return False
+            # if(visited[i]!=0) : continue #이미 방문한경우 but 인접한 정점과 다를경우
+            # else :  #방문안한경우
+            #     visited[i]=-visited[cur]
+            #     queue.append(i)
+            if visited[i] == 0:
+                visited[i] = -visited[cur]
                 queue.append(i)
+            elif visited[i] == visited[cur]:
+                return False
 
-            elif(visited[cur]==visited[i]) : #이미 방문햇엇음
-                    return False
     return True
-
 
 
 for _ in range(T) :
     V,E=map(int,input().split())
-    visited=[0]*(V+1) #방문체크
-    list1=[[] for _ in range(V+1)]
+    graph=[[]for _ in range(V+1)]
+
     for i in range(E) :
-        a,b=map(int,input().split())
-        list1[a].append(b)
-        list1[b].append(a)  #연결그래프 구현
+        a,b=map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
 
-    result=True
-
-    #다른 색깔로 칠해가면서 만약 같은색이 인접해잇으면 fals
-    for i in range(1, V+1) :
+    queue=deque()
+    check=True
+    visited = [0] * (V + 1)
+    for i in range(1,V+1) :
         if(visited[i]==0) :
-            result=BFS(i,visited)
+            result=BFS(i)
             if(not result) :
+                check=False
                 break
 
-
-    if(result): print("YES")
-    else:print("NO")
-
-
+    if(check) :
+        print("YES")
+    else :
+        print("NO")
 
 
 
