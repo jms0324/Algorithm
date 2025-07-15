fdc2a78366f06f6bace80b0e2128acf4bb614d0e
jwaminseok <109052897+jms0324@users.noreply.github.com>
2025-07-15 13:31:49 +0900

    [Silver III] Title: 파일 정리, Time: 264 ms, Memory: 131172 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -6,16 +6,13 @@ for i in range(N) :
     arr.append(input().split(".")[1])
 
 dict1={}
-list1=[]
-for i in arr :
-    if i in dict1 :
-        dict1[i]+=1
-    else :
-        dict1[i]=1
-        list1.append(i)
+for i in range(len(arr)) :
+    if (arr[i] in dict1):
+        dict1[arr[i]] += 1
+    elif(arr[i] not in dict1) : #새로운확장자
+        dict1[arr[i]]=1
 
-
-# list1=list(dict1.keys())
+list1=list(dict1.keys())
 list1.sort()
 for i in range(len(list1)) :
     print(str(list1[i])+" ")
