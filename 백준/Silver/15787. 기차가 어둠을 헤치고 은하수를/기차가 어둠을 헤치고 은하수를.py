3933539e8f267ecab69cd596872a77393ce270e5
jwaminseok <109052897+jms0324@users.noreply.github.com>
2025-07-15 14:26:42 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 248 ms, Memory: 134724 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
@@ -1,47 +1,43 @@
-N,M=map(int,input().split())
-train=[]
+import sys
+input=sys.stdin.readline
 
-for i in range(N):
-    train.append([])
+N,M=map(int,input().strip().split())
+train=[[0]*20 for _ in range(N)]
 
-    for j in range(20):
-        train[i].append(0)
-
-
-
-for i in range(M) : ##명령의수
-
-    command=list(map(int,input().split())) #명령하나
-
-    if(len(command)==3) :
+for i in range(M) : #명령
+    order=list(map(int,input().strip().split()))
+    if(order[0]==1) : #승차
+        if(train[order[1]-1][order[2]-1]==0) :
+            train[order[1]-1][order[2] - 1]=1
 
 
-        if(command[0]==1) :  #command[1]이 몇번째 열차인지 의미함 command[2]는 좌석
-            #승차
-            train[command[1]-1][command[2]-1]=1
+    elif(order[0]==2) : #하차
+        if (train[order[1] - 1][order[2] - 1] == 1):
+            train[order[1] - 1][order[2] - 1] -=1
 
-        else : #하차
-            train[command[1]-1][command[2]-1]=0
 
-    else :
+    elif(order[0]==3) : #한칸씩뒤로 +1
 
-        if(command[0]==3) :  #한칸씩뒤로 가
-
-            train[command[1]-1].pop()
-            train[command[1]-1].insert(0,0) #맨앞에 0 삽입
-
-        else : #한칸씩 땡기기
-            train[command[1]-1].pop(0) #맨앞제거
-            train[command[1]-1].append(0)#뒤에 0 삽입
+        for i in range(19,0,-1) :
+            train[order[1]-1][i]=train[order[1]-1][i-1]
+        train[order[1]-1][0]=0
 
 
 
 
-unique_trains = set()
-for t in train:
-    unique_trains.add(tuple(t))
 
-print(len(unique_trains))
+    elif(order[0]==4) : #한칸씩앞으로 -1
+
+        for i in range(0,19) :
+            train[order[1]-1][i]=train[order[1]-1][i+1]
+        train[order[1]-1][19]=0
+
+
+s=set(tuple(lst) for lst in train)
+
+print(len(s))
+
+
 
 
 
