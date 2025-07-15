410e2386713e66e1adb538c6998e2668be934cb5
jwaminseok <109052897+jms0324@users.noreply.github.com>
2025-07-15 22:16:26 +0900

    [Silver I] Title: 배열 돌리기, Time: 792 ms, Memory: 205000 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
@@ -1,47 +1,54 @@
-time = int(input())
-for _ in range(time) :
-    N,angle = map(int,input().split())
-    basic=[list(map(int,input().split()))  for _ in range(N)]
+import copy
+import sys
+input=sys.stdin.readline
+T=int(input().strip())
 
-    ans=[[0]* N for _ in range(N)]
 
-    if(angle ==0 or angle == 360) :
-        for i in basic :
+
+for _ in range(T) :
+    N, angle = map(int, input().strip().split())
+    graph=[]
+
+    for i in range(N) :
+        graph.append(list(map(int,input().strip().split())))
+
+
+
+    if(angle==360 or angle==0) : #그대로 출력
+        for i in graph :
             print(*i)
     else :
-        count=0
-        mid=N//2
-        if(angle<0) :
-            angle+=360
-            count=angle//45
-        else :
-            count=angle//45
+        if (angle < 0):
+            angle = 360 + angle
 
-
-        for k in range(count) :
-
-
+        angle = angle // 45
+        result = [[0] * N for i in range(N)]  # 결과를 옮겨담을 그래프
+        for w in range(angle):
             for i in range(N):
                 for j in range(N):
 
-                    if(i==j) :#주대각선 -> 가운데열
-                         ans[i][mid]=basic[i][j]
+                    if (i == j):  # 주대각선 -> 가운데 열
+                        result[i][N // 2] = graph[i][j]
 
-                    elif(j==mid) : #가운데열 -> 부대각선 i+j=N-1
-                        ans[i][N-i-1] = basic[i][j]
+                    elif (j == N // 2):  # 가운데열 -> 부대각선
+                        result[i][N - i - 1] = graph[i][j]
 
-                    elif(i+j==N-1) : #부대각선 -> 가운데행
-                        ans[mid][j] = basic[i][j]
+                    elif (i + j == N - 1):  # 부대각선 -> 가운데행
+                        result[N // 2][j] = graph[i][j]
 
-                    elif(i==mid) : #basic의 가운데행 -> 주대각선 i가 2일때
-                        ans[j][j] = basic[i][j]
-                    else :
-                        ans[i][j]=basic[i][j]
 
-            for x in range(N):
-                for y in range(N):
-                    basic[x][y]=ans[x][y]
+                    elif (i == N // 2):  # 가운데행 -> 주대각선
+                        result[j][j] = graph[i][j]
 
-        for a in basic  :
-            print(*a)
+                    else:  # 그외는 그냥 그대로
+                        result[i][j] = graph[i][j]
+
+            graph = copy.deepcopy(result)
+            result = [[0] * N for i in range(N)]
+
+        for i in range(N):
+            print(*graph[i])
+
+
+
 
